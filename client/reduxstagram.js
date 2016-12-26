8817f016b7d628bf30c7ae0f1a8e806344e27228
Megan Coyle <meg@mcoyle.com>
2016-12-26 09:24:52 -0800

    Add babel files

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,7 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+// Import css
+import css from './styles/style.styl';
+
+render(<p>hi</p>, document.getElementById('root'));
