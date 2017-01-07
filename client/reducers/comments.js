ecf0a07cfc8976d2aaace3a026b0cd0a24c099d8
Megan Coyle <meg@mcoyle.com>
2017-01-07 13:04:14 -0500

    Redux reducer composition

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,5 +1,33 @@
-function comments(state = [], action) {
+function postComments(state = [], action) {
+  switch(action.type){
+    case 'ADD_COMMENT':
+    // return the new state with the new comment
+      return [...state, {
+        user: action.author,
+        text: action.comment
+    }];
+    case 'REMOVE_COMMENT':
+      return [
+        // from the start to the one you want
+        ...state.slice(0,action.i),
+        // after the deleted one
+        ...state.slice(action.i + 1)
+      ]
+    default:
+      return state;
+  }
+  return state;
+}
 
+function comments(state = [], action) {
+  if(typeof action.postId !== 'undefined') {
+    return {
+      // take the current state
+      ...state,
+      // overwrite this post with a new one
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
   return state;
 }
 
